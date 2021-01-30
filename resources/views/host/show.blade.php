2e57bfabbbb27cc707e2f3c06882ba798f0025a8
Abdull2h <aboode.797@gmail.com>
2021-01-30 23:10:56 +0300

    update show event page

--- resources/views/host/show.blade.php
+++ resources/views/host/show.blade.php
@@ -5,38 +5,86 @@
     <div class="container my-2">
         <!-- First row -->
         <div class="row justify-content-center my-2">
-            <div class="h4 text-center">Event Detailes</div>
+            <div class="h4 text-center b-b">Event Detailes</div>
         </div>
 
         <!-- Second row -->
         <div class="row justify-content-center my-2">
-            <div class="col-md-8">
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item text-center"><img src="/public/images/{{ $event->image }}" alt="..."
-                            style="max-height: 400px; max-width: 400px;"></li>
-                    <li class="list-group-item">Name: {{ $event->name }}</li>
-                    <li class="list-group-item">Description: {{ $event->description }}</li>
-                    <li class="list-group-item">Location: {{ $event->location }}</li>
-                    <li class="list-group-item">Date: {{ $event->date }}</li>
-                    <li class="list-group-item">Time: {{ $event->time }}</li>
-                    <li class="list-group-item">Doorman: {{ App\Models\User::find($event->doorman)->name }}</li>
-                    <li class="list-group-item">
-                        <a href="/event/edit/{{ $event->id }}">
-                            <button class="btn btn-outline-warning btn-sm rounded pill">Edit</button>
-                        </a>
+            <div class="col-md-10">
+                <!-- TEST -->
+                <ul class="list-group list-group-horizontal b-l">
+                    <li class="list-group-item w-25 d-flex align-items-center">
+                        <strong>Event Image</strong>
                     </li>
-                    <li class="list-group-item"><a href="/event/show/{{ $event->id }}/add_invite">
-                            <button class="btn btn-outline-dark btn-sm rounded pill">Add Attendant</button></a>
-                    </li>
-                    <li class="list-group-item">
-                        <a href="/event/show/{{ $event->id }}/send_invitation">
-                            <button class="btn btn-outline-primary btn-sm rounded pill mx-1">Send Invitation to Attendants</button>
-                        </a>
-                        <a href="/event/show/{{ $event->id }}/assign_doorman">
-                            <button class="btn btn-outline-primary btn-sm rounded pill mx-1">Send Assignment to doorman</button>
-                        </a>
+                    <li class="list-group-item w-75 text-center"><img src="/public/images/{{ $event->image }}" alt="..."
+                            style="max-height: 300px; max-width: 300px;">
                     </li>
                 </ul>
+                <ul class="list-group list-group-horizontal b-l">
+                    <li class="list-group-item w-25 text-bold"><strong>Event Name:</strong></li>
+                    <li class="list-group-item w-75">{{ $event->name }}</li>
+                </ul>
+                <ul class="list-group list-group-horizontal b-l">
+                    <li class="list-group-item w-25 text-bold"><strong>Event
+                            Description:</strong></li>
+                    <li class="list-group-item w-75">{{ $event->description }}</li>
+                </ul>
+                <ul class="list-group list-group-horizontal b-l">
+                    <li class="list-group-item w-25 text-bold"><strong>Event Location:</strong>
+                    </li>
+                    <li class="list-group-item w-75">{{ $event->location }}</li>
+                </ul>
+                <ul class="list-group list-group-horizontal b-l">
+                    <li class="list-group-item w-25 text-bold"><strong>Event Date:</strong></li>
+                    <li class="list-group-item w-25">{{ $event->date }}</li>
+                    <li class="list-group-item w-25 text-bold"><strong>Event Time:</strong></li>
+                    <li class="list-group-item w-25">{{ $event->time }}</li>
+                </ul>
+                <ul class="list-group list-group-horizontal b-l">
+                    <li class="list-group-item w-25 text-bold"><strong>Event Owner:</strong></li>
+                    <li class="list-group-item w-25">
+                        {{ App\Models\User::find($event->owner)->name }}
+                    </li>
+                    <li class="list-group-item w-25 text-bold"><strong>Event Doorman:</strong>
+                    </li>
+                    <li class="list-group-item w-25">
+                        {{ App\Models\User::find($event->doorman)->name }}
+                    </li>
+                </ul>
+                @if ($event->owner == Auth()->user()->id || App\Models\Admin::where('user_id', Auth()->user()->id)->first())
+                    <ul class="list-group list-group-horizontal b-l">
+                        <li class="list-group-item w-50 text-center"><a href="/event/edit/{{ $event->id }}">
+                                <button class="btn btn-outline-dark btn-sm btn-block w-50 mx-auto rounded-pill"><i
+                                        class="bi bi-pencil-square align-top mr-2"></i>Edit</button>
+                            </a>
+                        </li>
+                        <li class="list-group-item w-50 text-center"><a href="/event/show/{{ $event->id }}/add_invite">
+                                <button class="btn btn-outline-dark btn-sm btn-block w-50 mx-auto rounded-pill"><i
+                                        class="bi bi-plus-square align-top mr-2"></i>Add
+                                    Attendant</button></a>
+                        </li>
+                    </ul>
+                @endif
+                @if ($event->owner == Auth()->user()->id)
+                    <ul class="list-group list-group-horizontal b-l">
+                        <li class="list-group-item w-50 text-center"><a
+                                href="/event/show/{{ $event->id }}/send_invitation">
+                                <button class="btn btn-primary btn-sm w-50 mx-auto rounded-pill"><i
+                                        class="bi bi-envelope align-top mr-2"></i>Send
+                                    Invitation to
+                                    Attendants</button>
+                            </a></li>
+                        <li class="list-group-item w-50 text-center">
+                            <a href="/event/show/{{ $event->id }}/assign_doorman">
+                                <button class="btn btn-primary btn-sm w-50 mx-auto rounded-pill"><i
+                                        class="bi bi-door-open align-top mr-2"></i>Send
+                                    Assignment to
+                                    doorman</button>
+                            </a>
+                        </li>
+                    </ul>
+                @endif
+
             </div>
         </div>
 
@@ -47,7 +95,7 @@
 
         <!-- Fourth row -->
         <div class="row justify-content-center my-2">
-            <div class="col-md-8">
+            <div class="col-md-10">
                 <div class="row m-2 align-items-end">
                     <label for="search" class="form-label m-2">Find Attendant</label>
                     <input type="text" id="search" class="form-control w-50 mx-auto rounded-pill" placeholder="Search..">
@@ -69,9 +117,12 @@
                                 <td>{{ $attendant->email }}</td>
                                 <td>{{ $attendant->seats }}</td>
                                 <td>{{ str_pad($attendant->code, 6, '0', STR_PAD_LEFT) }}</td>
-                                <td><a class="btn btn-sm btn-primary rounded-pill"
-                                        href="/event/show/{{ $attendant->event_id }}/edit_invite/{{ $attendant->id }}"><i class="bi bi-pencil-square align-top mr-1"></i>Edit</a>
-                                </td>
+                                @if ($event->owner == Auth()->user()->id || App\Models\Admin::where('user_id', Auth()->user()->id)->first())
+                                    <td><a class="btn btn-sm btn-primary rounded-pill btn-block"
+                                            href="/event/show/{{ $attendant->event_id }}/edit_invite/{{ $attendant->id }}"><i
+                                                class="bi bi-pencil-square align-top mr-1"></i>Edit</a>
+                                    </td>
+                                @endif
                             </tr>
                         @endforeach
                     </tbody>
