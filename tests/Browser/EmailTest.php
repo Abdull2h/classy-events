7c74c2e9ac6b1b3a562d95a26107b637dafd278b
ahmad17laban <60614075+ahmad17laban@users.noreply.github.com>
2021-02-14 13:11:39 +0300

    Email functinalty test in

---
+++ tests/Browser/EmailTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class EmailTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+            ->type('name','ahmad abulaban')
+            ->type('email', 'ahmad!8laban@gmial.com')
+            ->type('subject', 'loremlorememsum')
+            ->type('message', 'loremlorememsumloremlorememsumloremlorememsum')
+            ->press('Send Email');
+        });
+    }
+}
